cff439c9b62b7bb7883fc40f50d913bbb34b0597
euberman <euberman@gmail.com>
2020-12-04 13:35:46 -0600

    setup html templates

--- fbo-workorder-frontend/src/index.js
+++ fbo-workorder-frontend/src/index.js
@@ -1,11 +1,11 @@
 const BASE_URL = "http://localhost:3000"
 const _headers = { 'Access-Control-Allow-Origin', "Content-Type": "application/json", Accept: "application/json" }
-let parentContainer;
+let mainContainer;
 let activeWorkorder;
 // ========================================================================
 
 document.addEventListener("DOMContentLoaded", () => {
-  parentContainer = document.getElementById('parentContainer')
+  mainContainer = document.getElementById('main-container')
   fetchWorkorders()
   linkNavButtons()
 });
@@ -14,16 +14,34 @@ function linkNavButtons(){
   const dashboardBtn = document.getElementById('dashboardLink')
     dashboardBtn.addEventListener('click', renderDashboard)
   const workordersBtn = document.getElementById('workordersLink')
-  workordersBtn.addEventListener('click', renderDashboard)
+  workordersBtn.addEventListener('click', renderWorkorders)
   const servicesBtn = document.getElementById('servicesLink')
-  servicesBtn.addEventListener('click', renderDashboard)
+  servicesBtn.addEventListener('click', renderServices)
   const customersBtn = document.getElementById('customersLink')
-  customersBtn.addEventListener('click', renderDashboard)
+  customersBtn.addEventListener('click', renderCustomers)
 }
 // ========================================================================
 // DASHBOARD
 function renderDashboard(){
-  parentContainer.innerHTML = '';
+  mainContainer.innerHTML = '';
+  let dashboardTemplate = `
+      <div class="level mt-2 mr-2 content-title"><div class="level-left"><div class="level-item title">Dashboard</div></div></div>
+      <div class="columns is-multiline">
+            <div class="column"><div class="box notification is-primary"><div class="heading">Top Seller Total</div><div class="title">56,950</div></div></div>
+            <div class="column"><div class="box notification is-warning"><div class="heading">Revenue / Expenses</div><div class="title">55% / 45%</div></div></div>
+            <div class="column"><div class="box notification is-info"><div class="heading">Feedback Activity</div><div class="title">78% ↑</div></div></div>
+            <div class="column"><div class="box notification is-danger"><div class="heading">Orders / Returns</div><div class="title">75% / 25%</div></div></div>    
+      </div> 
+      <div class="columns is-multiline">
+            <div class="column is-12">
+              <article class="message is-dark">
+                <div class="message-header"> Chart </div>
+                <div class="message-body" style="position: relative;"></div>
+              </article>
+            </div>
+      </div>
+    `;
+  mainContainer.append(dashboardTemplate);
 
 }
 
